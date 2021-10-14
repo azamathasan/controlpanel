3aa4da7212d31f0c487a575e2fbd9e6d45c9d47b
azamathasan <azik-1988@mail.ru>
2021-10-15 00:36:22 +0500

    dist production for github pages (vue.js instruction)3

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,3 @@
-/* module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/controlpanel/'
-      : '/'
-  } */
 module.exports = {
     publicPath: '/controlpanel/'
   }
